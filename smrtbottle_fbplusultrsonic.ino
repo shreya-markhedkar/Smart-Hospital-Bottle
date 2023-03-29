03d7f7c20f46376fea007c19b82930fbd4e71c12
Shreya Markhedkar <81920073+shreya-markhedkar@users.noreply.github.com>
2023-03-29 08:07:07 +0530

    Update blynk,thingspeak
    Updated with blynk,thingspeak

--- smrtbottle_fbplusultrsonic.ino
+++ smrtbottle_fbplusultrsonic.ino
@@ -1,6 +1,8 @@
 
 #include <WiFi.h>
 #include <Firebase_ESP_Client.h>
+#include "ThingSpeak.h"
+#include <BlynkSimpleEsp32.h>
 ////////////////////////
 #define trigger_pin 5
 #define Echo_pin 18
@@ -9,6 +11,12 @@
 long duration;
 int distance;
 /////////////////////
+#define BLYNK_TEMPLATE_ID           "TMPLC7ZNN-Kd"
+#define BLYNK_TEMPLATE_NAME         "esp32"
+#define BLYNK_AUTH_TOKEN            "bNi9T2Qm1ZVhocuq2-1quphioy_GHNeK"
+char authz[] = BLYNK_AUTH_TOKEN;
+BlynkTimer timer;
+//////////////
 //Provide the token generation process info.
 #include "addons/TokenHelper.h"
 //Provide the RTDB payload printing info and other helper functions.
@@ -18,7 +26,9 @@ int distance;
 
 #define WIFI_SSID "Shreya's Galaxy A52s 5G"
 #define WIFI_PASSWORD "rrfg9048"
-
+WiFiClient  client;
+unsigned long myChannelNumber = 2085125;
+const char * myWriteAPIKey = "HF5VFXUNZCHL3E0Q";
 // Insert Firebase project API Key
 #define API_KEY "AIzaSyDWR9NOlbsZOCs78ppPgAI3R6NYGMJccMM"
 
@@ -34,9 +44,21 @@ FirebaseConfig config;
 unsigned long sendDataPrevMillis = 0;
 int count = 0;
 bool signupOK = false;
+/////////////////
+unsigned long lastTime = 0;
+unsigned long timerDelay = 30000;
+float v;
+////////////////////////
+void waterlevel(){
 
+}
+/////////////////////
 void setup(){
   Serial.begin(115200);
+  ////
+    WiFi.mode(WIFI_STA);   
+  ThingSpeak.begin(client);
+  ////
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED){
@@ -73,6 +95,7 @@ void setup(){
 pinMode(LED, OUTPUT); // Set the LED (D13) pin as a digital output
 pinMode(Echo_pin, INPUT); // configure the Echo_pin(D11) as an Input
   //////////////////////////
+  Blynk.begin(authz, WIFI_SSID, WIFI_PASSWORD);
 }
 
 void loop(){
@@ -89,7 +112,9 @@ digitalWrite(trigger_pin, LOW);   // make trigger pin active low
 /*Measure the Echo output signal duration or pulss width */
 duration = pulseIn(Echo_pin, HIGH); // save time duration value in "duration variable
 distance= duration*0.034/2; //Convert pulse duration into distance
-
+////
+  Blynk.virtualWrite(V1, (17-distance));
+/////
 /* if distance greater than 10cm, turn on LED */
 // if ( (17-distance) == 0||(17-distance<2)) {
 // Serial.print("Empty ");
@@ -109,6 +134,18 @@ distance= duration*0.034/2; //Convert pulse duration into distance
 // if (distance > 18)
 // Serial.println("Please insert bottle.");
 // delay(1000);
+ int x = ThingSpeak.writeField(myChannelNumber, 1, (17-distance), myWriteAPIKey);
+ if(x == 200){
+      Serial.println("Channel update successful.");
+    }
+    else{
+      Serial.println("Problem updating channel. HTTP error code " + String(x));
+    }
+    lastTime = millis();
+    ///////////////////////////////
+     
+  Blynk.run();
+  timer.run();
   /////////////////////////////////
   if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
     sendDataPrevMillis = millis();
@@ -116,33 +153,38 @@ distance= duration*0.034/2; //Convert pulse duration into distance
 // ////////
 if(distance>17){
   lert=0;
+  digitalWrite(4,HIGH);
  if (Firebase.RTDB.setInt(&fbdo, "level/int", -1)){
       Serial.println("PASSED");
+      
       // Serial.println("PATH: " + fbdo.dataPath());
       // Serial.println("TYPE: " + fbdo.dataType());
     }
     else {
       Serial.println("FAILED");
       Serial.println("REASON: " + fbdo.errorReason());
+      
     }  
     if (Firebase.RTDB.setInt(&fbdo, "level/alert",lert )){
       Serial.println("PASSED");
-      // Serial.println("PATH: " + fbdo.dataPath());
-      // Serial.println("TYPE: " + fbdo.dataType());
+      
     }
     else {
       Serial.println("FAILED");
       Serial.println("REASON: " + fbdo.errorReason());
     }
-    delay(1000);
+    delayMicroseconds(2);
 }
 ///////////////////
 if((distance>=15)&&(distance<=17)){
   lert=1;
+  //digitalWrite(19,HIGH);
+  tone(19, 1000);
+  Blynk.logEvent("test_event");
+  digitalWrite(4,HIGH);
   if (Firebase.RTDB.setInt(&fbdo, "level/int", 17-distance)){
       Serial.println("PASSED");
-      // Serial.println("PATH: " + fbdo.dataPath());
-      // Serial.println("TYPE: " + fbdo.dataType());
+     
     }
     else {
       Serial.println("FAILED");
@@ -150,22 +192,20 @@ if((distance>=15)&&(distance<=17)){
     }
     if (Firebase.RTDB.setInt(&fbdo, "level/alert",lert )){
       Serial.println("PASSED");
-      // Serial.println("PATH: " + fbdo.dataPath());
-      // Serial.println("TYPE: " + fbdo.dataType());
+      
     }
     else {
       Serial.println("FAILED");
       Serial.println("REASON: " + fbdo.errorReason());
     }
-    delay(1000);
+    delayMicroseconds(2);
 }
 ///////////////////
 if(distance<15 && distance>0){
   lert=0;  
+  digitalWrite(4,HIGH);
    if (Firebase.RTDB.setInt(&fbdo, "level/int", 17-distance)){
       Serial.println("PASSED");
-      // Serial.println("PATH: " + fbdo.dataPath());
-      // Serial.println("TYPE: " + fbdo.dataType());
     }
     else {
       Serial.println("FAILED");
@@ -173,22 +213,19 @@ if(distance<15 && distance>0){
     }
      if (Firebase.RTDB.setInt(&fbdo, "level/alert",lert )){
       Serial.println("PASSED");
-      // Serial.println("PATH: " + fbdo.dataPath());
-      // Serial.println("TYPE: " + fbdo.dataType());
     }
     else {
       Serial.println("FAILED");
       Serial.println("REASON: " + fbdo.errorReason());
     }
-    delay(1000);
+    delayMicroseconds(2);
 }
 ///////////////////
 if(distance==0){
   lert=0;  
    if (Firebase.RTDB.setInt(&fbdo, "level/int", 17)){
       Serial.println("PASSED");
-      // Serial.println("PATH: " + fbdo.dataPath());
-      // Serial.println("TYPE: " + fbdo.dataType());
+     
     }
     else {
       Serial.println("FAILED");
@@ -196,16 +233,13 @@ if(distance==0){
     }
      if (Firebase.RTDB.setInt(&fbdo, "level/alert",lert )){
       Serial.println("PASSED");
-      // Serial.println("PATH: " + fbdo.dataPath());
-      // Serial.println("TYPE: " + fbdo.dataType());
     }
     else {
       Serial.println("FAILED");
       Serial.println("REASON: " + fbdo.errorReason());
     }
-    delay(1000);
+    delayMicroseconds(2);
 }
-///////////////////
     
   }
 }
